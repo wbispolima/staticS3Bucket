bd1190976ff1c6ae1b53c2f97cd4f96bde86ada8
Waldinei Bispo <waldinei.bispo@ifmt.edu.br>
2024-04-30 09:02:49 -0400

    corrigido ponto

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -78,7 +78,7 @@ resource "aws_s3_bucket_versioning" "my-versioning" {
   }
 }
 
-resource "aws_s3_object" "index.html" {
+resource "aws_s3_object" "index_html" {
   bucket       = aws_s3_bucket.mybucket.id
   key          = "index.html"
   source       = "site/index.html"
